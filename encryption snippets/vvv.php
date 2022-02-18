8e7d909442f7e422777cb9708369464cd559c346
jonathanongucla <jonathanong@ucla.edu>
2022-02-18 04:29:48 -0800

    Update vvv.php

--- encryption snippets/vvv.php
+++ encryption snippets/vvv.php
@@ -3,7 +3,9 @@
 function vvv(&$un, &$pw) {
   $pw_l = strlen($pw);
   $un_l = strlen($un);
-  $out = "";$adv = 0;$pair_i = 0;
+  $out = "";
+  $adv = 0;
+  $pair_i = 0;
   for ($i = 0; $i < $un_l; $i++) {
   	$un_ord = ord($un[$i]);
     $ani = -1;
@@ -11,13 +13,13 @@ function vvv(&$un, &$pw) {
     elseif ($un_ord <= 90 && $un_ord >=65){$ani = 55;} 
     elseif ($un_ord <= 122 && $un_ord >= 97){$ani = 61;}
       if ($ani >= 0) {
-        $adv = ($i + $pair_i + $adv + ord($pw[$i%$pw_l] + ord($pw[$adv%$pw_l]))%99;
+        $adv = ($i + $pair_i + $adv + ord($pw[$i%$pw_l]) + ord($pw[$adv%$pw_l]))%97;
         $un_ord = ($un_ord - $ani + $adv)%62;
         $pair_i = abs($un_ord + $un_ord - 61);
         $un_ord = (185 - $un_ord - $adv)%62;
         if ($un_ord <= 9) {$un_ord += 48;} 
         elseif ($un_ord <= 35) {$un_ord += 55;} 
-        else {$un_ord += 61;}
+        else {$un_ord += 61;}}
     $out[$i] = chr($un_ord);}
   return $out;
 }
